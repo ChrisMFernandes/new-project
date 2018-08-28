f0a68f674ac61a49c00e3fcf0b73265f23d2d995
ChrisMFernandes <Chrismfernandes12@gmail.com>
2018-08-28 12:07:09 -0400

    adding to post controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,2 +1,50 @@
 class PostsController < ApplicationController
+
+  def index 
+    @post = Post.all
+  end
+
+  def new 
+    @post = Post.new
+  end
+
+  def create 
+    @post = Post.new(post_params)
+
+    respond_to do |format|
+      if @post.save
+          format.html { redirect_to posts_path, notice: 'Post was successfully created, yo!' }
+          format.json { render :index, status: :created, location: posts_path }
+          format.js
+      else
+          format.html { render :new }
+          format.json { render json: @post.errors, status: :unprocessable_entity }
+          format.js
+      end
+    end
+  end
+
+  def edit
+		@post = Post.find(params[:id])
+  end
+  
+  def update
+		@post = Post.find(params[:id])
+		@post.update(post_params)
+		redirect_to posts_path
+  end
+  
+  def destroy
+		@post = Post.find(params[:id])
+		@post.destroy
+		redirect_to posts_path
+	end
+
+
+private
+
+  def post_params
+    params.require(:post).permit(:title, :body)
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,7 @@ Rails.application.routes.draw do
   
   root to: "home#index"
 
+  get "about", to: "home#about"
+
   resources :posts
 end
