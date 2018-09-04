02fbf96d3e425e16f240ae6a94aceb815394e98e
ChrisMFernandes <Chrismfernandes12@gmail.com>
2018-09-04 14:53:29 -0400

    working on post index

--- db/migrate/20180827194129_create_posts.rb
+++ db/migrate/20180827194129_create_posts.rb
@@ -4,6 +4,9 @@ class CreatePosts < ActiveRecord::Migration[5.2]
       t.string :title
       t.text :body
       t.string :author
+      
+      t.datetime "created_at", null: false
+      t.datetime "updated_at", null: false
       t.references :user, index: true, foreign_key: true
     end
   end
--- db/schema.rb
+++ db/schema.rb
@@ -16,6 +16,8 @@ ActiveRecord::Schema.define(version: 2018_08_27_194129) do
     t.string "title"
     t.text "body"
     t.string "author"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.integer "user_id"
     t.index ["user_id"], name: "index_posts_on_user_id"
   end
